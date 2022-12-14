9b27e3f870d9e4cb9d8463c173555c97ca1f8437
geekyWomaniya <geekyGit@786>
2022-12-14 08:22:52 -0600

    Day1: Algo: Strings(1. Palindrome)

--- Algo/Strings/palindrome.py
+++ Algo/Strings/palindrome.py
@@ -1,1 +1,14 @@
 # Palindrome
+
+def isPalindrome(inputString):
+    for i in range(int(len(inputString)/2)):
+        # print(inputString[-i-1])
+        if inputString[-i-1] != inputString[i]:
+            return False
+    return True
+
+inputString = "abca"
+
+output = isPalindrome(inputString)
+
+print(output)
